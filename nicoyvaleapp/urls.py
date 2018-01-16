be3b19634fba83c3eedf58e9cc854324f51816d4
alejandro.mesias <alejandromesiasu@gmail.com>
2018-01-15 23:24:48 -0500

    first template modification for nico y vale

--- nicoyvaleapp/urls.py
+++ nicoyvaleapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from nicoyvaleapp import views
 
 urlpatterns = [
-    url(r'^$', views.index, name ='index'),
+    url(r'^$', views.home, name ='home'),
     url(r'^modelo', views.modelo, name ='modelo'),
 ]
--- nicoyvaleapp/views.py
+++ nicoyvaleapp/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'nicoyvaleapp/nyv_home.html')
+
 def index(request):
     text = {'inserted_text': "hola texto insertadoss"}
     return render(request, 'nicoyvaleapp/index.html', context = text)
 
 def modelo(request):
-    return render(request, 'nicoyvaleapp/modelo.html')  
+    return render(request, 'nicoyvaleapp/modelo.html')
 
 def intro(request):
     return render(request, 'celebrar/celebrar_home.html',)
