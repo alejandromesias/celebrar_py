c84fcc4816a4c9b1ad704adaace36f90456e41d0
alejandro.mesias <alejandromesiasu@gmail.com>
2018-01-17 13:28:08 -0500

    seccion ceremonia

--- nicoyvaleapp/views.py
+++ nicoyvaleapp/views.py
@@ -8,11 +8,18 @@ def home(request):
     historia_parr1 = "Hay veces en la vida donde las cosas buenas llegan sin avisar y cuando menos lo esperas. Nos pasó a nosotros. Nos conocimos hace casi 9 años, pero en ese tiempo cada uno tenía un rumbo diferente... "
     historia_parr2 = "Aunque en ese momento el destino nos separó, 4 años más tarde nuestras vidas eran distintas y al encontrarnos nuevamente comenzó ésta historia. Desde ahí hemos caminado juntos, viviendo momentos únicos, aceptando quienes somos y apoyándonos en cada decisión."
     historia_parr3 = "Hace 7 meses decidimos juntar nuestras vidas para siempre y nos embarcamos en esta nueva aventura, con más retos y objetivos comunes. Estamos felices de poder compartir el inicio de ésta etapa junto a quienes más queremos."
+    nombre_ceremonia = "Parroquia La Sagrada Familia del Condado"
+    direccion_ceremonia = 'Gonzalo Cordero S/N y Ricardo Descalzi, Urb. "El Condado", Quito'
+    hora_ceremonia = "Sábado 3  de marzo, 12h00"
 
-    context_dic = {'marquesina': "testing",
+    context_dic = {'marquesina': marquesina,
                 'historia_parr1': historia_parr1,
                 'historia_parr2': historia_parr2,
-                'historia_parr3': historia_parr3,}
+                'historia_parr3': historia_parr3,
+                'nombre_ceremonia':nombre_ceremonia,
+                'direccion_ceremonia':direccion_ceremonia,
+                'hora_ceremonia':hora_ceremonia,
+                }
     return render(request, 'nicoyvaleapp/nyv_home.html', context = context_dic)
 
 def index(request):
