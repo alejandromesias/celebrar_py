80829be71516f4e06204a2d0e42c33dfefd62df4
alejandro.mesias <alejandromesiasu@gmail.com>
2018-01-17 19:05:41 -0500

    seccion recepcion

--- nicoyvaleapp/views.py
+++ nicoyvaleapp/views.py
@@ -8,9 +8,15 @@ def home(request):
     historia_parr1 = "Hay veces en la vida donde las cosas buenas llegan sin avisar y cuando menos lo esperas. Nos pasó a nosotros. Nos conocimos hace casi 9 años, pero en ese tiempo cada uno tenía un rumbo diferente... "
     historia_parr2 = "Aunque en ese momento el destino nos separó, 4 años más tarde nuestras vidas eran distintas y al encontrarnos nuevamente comenzó ésta historia. Desde ahí hemos caminado juntos, viviendo momentos únicos, aceptando quienes somos y apoyándonos en cada decisión."
     historia_parr3 = "Hace 7 meses decidimos juntar nuestras vidas para siempre y nos embarcamos en esta nueva aventura, con más retos y objetivos comunes. Estamos felices de poder compartir el inicio de ésta etapa junto a quienes más queremos."
+
     nombre_ceremonia = "Parroquia La Sagrada Familia del Condado"
     direccion_ceremonia = 'Gonzalo Cordero S/N y Ricardo Descalzi, Urb. "El Condado", Quito'
     hora_ceremonia = "Sábado 3  de marzo, 12h00"
+
+    nombre_recepcion = "Hacienda Villa Vieja"
+    direccion_recepcion = "Juan Procel y José Miguel Carrión, sector El Condado"
+    hora_recepcion = "3 de marzo, a partir de las 13h00"
+    web_recepcion = "http://haciendavillavieja.com"
 
     context_dic = {'marquesina': marquesina,
                 'historia_parr1': historia_parr1,
@@ -19,6 +25,10 @@ def home(request):
                 'nombre_ceremonia':nombre_ceremonia,
                 'direccion_ceremonia':direccion_ceremonia,
                 'hora_ceremonia':hora_ceremonia,
+                'nombre_recepcion':nombre_recepcion,
+                'direccion_recepcion':direccion_recepcion,
+                'hora_recepcion':hora_recepcion,
+                'web_recepcion':web_recepcion,
                 }
     return render(request, 'nicoyvaleapp/nyv_home.html', context = context_dic)
 
